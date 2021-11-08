d2b787e0b851a6d80cba451eae25a9f992fda6d1
quling <quling@dist.com.cn>
2021-11-08 21:42:49 +0800

    安装iview插件

--- src/App.vue
+++ src/App.vue
@@ -5,36 +5,43 @@
     <router-view />
   </div>-->
 
-  <div>
+  <!-- <div>
     <ifAndShow />
     <props :minLength="2" />
     <myslot>你好</myslot>
+  </div>-->
+  <div>
+    <Scroll :on-reach-bottom="handleReachBottom">
+      <Card
+        dis-hover
+        v-for="(item, index) in list1"
+        :key="index"
+        style="margin: 32px 0"
+      >Content {{ item }}</Card>
+    </Scroll>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import ifAndShow from "@/UnitTest/ifAndShow/ifAndShow.vue";
-import props from "@/UnitTest/props/props.vue";
-import myslot from "@/UnitTest/slot/slot.vue";
 export default {
-  name: "App",
-  components: {
-    ifAndShow,
-    props,
-    myslot
-    // HelloWorld
+  data() {
+    return {
+      list1: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    };
+  },
+  methods: {
+    handleReachBottom() {
+      return new Promise((resolve) => {
+        console.log(1);
+        //setTimeout(() => {
+        const last = this.list1[this.list1.length - 1];
+        for (let i = 1; i < 11; i++) {
+          this.list1.push(last + i);
+        }
+        resolve();
+        //   }, 2000);
+      });
+    }
   }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
